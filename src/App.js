acf7d182f766035743116f810aa7cd9f48882ab1
Rohan <>
2022-10-26 14:17:54 +0530

    added advices

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./app.css"
 
 function App() {
+  const [data,setData] = useState("In order to get, you have to give.")
+
+  const getadvice = () =>{
+    axios.get("	https://api.adviceslip.com/advice")
+    .then((result)=>{
+      setData(result.data.slip.advice)
+      // console.log(result)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+
   return (
+
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+        <div class="content">
+            <h1>{data}</h1>
+            <button class="button" onClick={getadvice}><span>Get the Advices here</span></button>
+        </div>
     </div>
   );
 }
